162dd9ec0fe47252027718f7a11ae6193ecb68a1
sotoalexs901 <sotoalexs90@gmail.com>
2025-11-12 16:56:55 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
-VITE_FIREBASE_API_KEY=AIzaSyDw3sc9HkyHzme0RrZPqfPDOPnOxdQ_Es8
-VITE_FIREBASE_AUTH_DOMAIN=tpa-schedule.firebaseapp.com
-VITE_FIREBASE_PROJECT_ID=tpa-schedule
-VITE_FIREBASE_STORAGE_BUCKET=tpa-schedule.appspot.com
-VITE_FIREBASE_MESSAGING_SENDER_ID=50257694129
-VITE_FIREBASE_APP_ID=1:50257694129:web:c5e8081584b7c30a29c190
-
-}
-
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
