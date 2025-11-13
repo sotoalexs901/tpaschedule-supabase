96f8b07de404f9f0aa5436b4c57122430fe5a136
sotoalexs901 <sotoalexs90@gmail.com>
2025-11-13 14:49:21 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,15 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "tpa-schedule.firebaseapp.com",
-  projectId: "tpa-schedule",
-  storageBucket: "tpa-schedule.firebasestorage.app",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// FORZAR USO DEL BUCKET NO ESTÁNDAR
-export const storage = getStorage(app, "gs://tpa-schedule.firebasestorage.app");
-
-// Firestore
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
