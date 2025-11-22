82b53d980523884c0643f388bf5711e0a9759f0b
sotoalexs901 <sotoalexs90@gmail.com>
2025-11-21 19:48:18 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './styles.css'
+
 import { UserProvider, useUser } from './UserContext.jsx'
+
 import LoginPage from './pages/LoginPage.jsx'
 import AppLayout from './pages/AppLayout.jsx'
 import DashboardPage from './pages/DashboardPage.jsx'
@@ -14,13 +16,17 @@ import ApprovalsPage from './pages/ApprovalsPage.jsx'
 import BudgetsPage from './pages/BudgetsPage.jsx'
 import ApprovedSchedulesPage from './pages/ApprovedSchedulesPage.jsx'
 import ApprovedScheduleView from "./pages/ApprovedScheduleView.jsx"
-import CreateUserPage from "./pages/CreateUserPage.jsx";
-import EditUsersPage from "./pages/EditUsersPage.jsx";
+import CreateUserPage from "./pages/CreateUserPage.jsx"
+import EditUsersPage from "./pages/EditUsersPage.jsx"
+
 
 function ProtectedRoute({ children, roles }) {
   const { user } = useUser()
+
   if (!user) return <Navigate to="/login" replace />
+
   if (roles && !roles.includes(user.role)) return <Navigate to="/" replace />
+
   return children
 }
 
@@ -29,10 +35,10 @@ function AppRouter() {
     <BrowserRouter>
       <Routes>
 
-        {/* Login */}
+        {/* LOGIN */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* Main layout */}
+        {/* MAIN LAYOUT */}
         <Route
           path="/"
           element={
@@ -46,7 +52,7 @@ function AppRouter() {
           <Route index element={<DashboardPage />} />
           <Route path="dashboard" element={<DashboardPage />} />
 
-          {/* Dashboard Editor - Only Station Manager */}
+          {/* Dashboard Editor */}
           <Route
             path="dashboard-editor"
             element={
@@ -79,7 +85,7 @@ function AppRouter() {
             }
           />
 
-          {/* Approvals (only Station Manager) */}
+          {/* Approvals (ONLY station manager) */}
           <Route
             path="approvals"
             element={
@@ -89,7 +95,7 @@ function AppRouter() {
             }
           />
 
-          {/* Approved schedules (Station Manager + Duty Manager) */}
+          {/* Approved schedules (station + duty managers) */}
           <Route
             path="approved"
             element={
@@ -119,28 +125,33 @@ function AppRouter() {
             }
           />
 
+          {/* CREATE USER */}
+          <Route
+            path="create-user"
+            element={
+              <ProtectedRoute roles={['station_manager']}>
+                <CreateUserPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* EDIT USERS */}
+          <Route
+            path="edit-users"
+            element={
+              <ProtectedRoute roles={['station_manager']}>
+                <EditUsersPage />
+              </ProtectedRoute>
+            }
+          />
+
         </Route>
-<Route
-  path="create-user"
-  element={
-    <ProtectedRoute roles={["station_manager"]}>
-      <CreateUserPage />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="edit-users"
-  element={
-    <ProtectedRoute roles={['station_manager']}>
-      <EditUsersPage />
-    </ProtectedRoute>
-  }
-/>
 
       </Routes>
     </BrowserRouter>
   )
 }
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
