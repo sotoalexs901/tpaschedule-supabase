eb72877b48b7bc107c3d9c1fccb72a1e6385cf04
sotoalexs901 <sotoalexs90@gmail.com>
2026-09-01 15:58:37 -0400

    Update APP_NAME from 'AeroOps Hub' to 'AeroStation Hub'

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,4 +1,4 @@
-export const APP_NAME = "AeroOps Hub";
+export const APP_NAME = "AeroStation Hub";
 export const APP_SUBTITLE = "Operational Management Platform";
 export const APP_OWNER = "ANapoles Solutions";
 export const APP_POWERED_BY = "Powered by ANapoles Solutions";
