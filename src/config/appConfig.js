cb2548e58df59b3f90655b15b81afb14f32a12a1
sotoalexs901 <sotoalexs90@gmail.com>
2026-09-01 15:44:29 -0400

    Create appConfig.js

---
+++ src/config/appConfig.js
@@ -0,0 +1,5 @@
+export const APP_NAME = "AeroOps Hub";
+export const APP_SUBTITLE = "Operational Management Platform";
+export const APP_OWNER = "ANapoles Solutions";
+export const APP_POWERED_BY = "Powered by ANapoles Solutions";
+export const APP_COPYRIGHT = "© 2026 ANapoles Solutions. All rights reserved.";
