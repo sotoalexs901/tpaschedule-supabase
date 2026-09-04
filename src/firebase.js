3da0ecab661716d9a4fef7b07f2c872ca0504178
sotoalexs901 <sotoalexs90@gmail.com>
2026-09-04 00:31:07 -0400

    Refactor app initialization in firebase.js
    Changed export of app to a local constant.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // src/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -13,7 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
