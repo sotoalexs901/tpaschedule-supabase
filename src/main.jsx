86fbe1b4b42eae0df6f375f30800035c856bf2e9
sotoalexs901 <sotoalexs90@gmail.com>
2026-09-14 16:14:59 -0400

    Change import extension from .js to .jsx

--- src/main.jsx
+++ src/main.jsx
@@ -85,7 +85,7 @@ import WchrPoiReportsAdminPage from "./pages/WchrPoiReportsAdminPage.jsx";
 import SupervisorRegulatedGarbagePage from "./pages/SupervisorRegulatedGarbagePage.jsx";
 import RegulatedGarbageAdminPage from "./pages/RegulatedGarbageAdminPage.jsx";
 import RegulatedGarbageCompliancePage from "./pages/RGCompliancePage.jsx";
-import RegulatedGarbageComplianceAdminPage from "./pages/RGComplianceAdminPage.js";
+import RegulatedGarbageComplianceAdminPage from "./pages/RGComplianceAdminPage.jsx";
 
 import MonthlyEmployeePerformanceReportPage from "./pages/MonthlyEmployeePerformanceReportPage.jsx";
 import EmployeePerformanceManagementPage from "./pages/EmployeePerformanceManagementPage.jsx";
