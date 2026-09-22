ec9429cf7cac110f59614bf726be0311143d21b9
sotoalexs901 <sotoalexs90@gmail.com>
2026-09-21 20:47:44 -0400

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,11 +1,8 @@
-/** @type {import("@capacitor/cli").CapacitorConfig} */
-const config = {
-  appId: "com.anapolessolutions.aerostationhub",
-  appName: "AeroStation Hub",
-  webDir: "dist",
-  server: {
-    androidScheme: "https"
+{
+  "appId": "com.anapolessolutions.aerostationhub",
+  "appName": "AeroStation Hub",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https"
   }
-};
-
-module.exports = config;
+}
